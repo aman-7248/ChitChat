9ac77f2e0edb698af505679126fb0aec5cd7a2b7
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2025-07-11 13:38:11 +0530

    sign up route done and jwt

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,8 @@ dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 4000;
-app.get("/hi",(req,res)=>{
-    res.send("hello");
-})
 
-
+app.use(express.json());
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
