a226c28994dcdb2ecab6171e7d0b971eeee01b3f
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2025-07-25 17:23:26 +0530

    realtime feature bugs resolved

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,40 +1,58 @@
-import {useChatStore} from "../store/useChatStore"
-import {useAuthStore} from "../store/useAuthStore"
-import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { useEffect, useRef } from "react";
+
 import ChatHeader from "./ChatHeader";
-import MessageInput from "./MessageInput"
+import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
-const ChatContainer=()=>{
-  const {messages,getMessages,isMessagesLoading,selectedUser}=useChatStore();
-
+const ChatContainer = () => {
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
   const { authUser } = useAuthStore();
-  
-  useEffect(()=>{
-    getMessages(selectedUser._id)
-  },[selectedUser._id,getMessages])
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+
+    subscribeToMessages();
+
+    return () => unsubscribeFromMessages();
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   if (isMessagesLoading) {
     return (
       <div className="flex-1 flex flex-col overflow-auto">
-        <ChatHeader /> {/*down also this component */}
-        <MessageSkeleton />  {/*extra compoent to show message loading */}
-        <MessageInput />  {/*down also this component */}
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
       </div>
     );
   }
-  
 
-   return (
+  return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
-      {/*displaying messages btw 2 guys */}
+
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.map((message) => (
           <div
             key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
           >
             <div className=" chat-image avatar">
               <div className="size-10 rounded-full border">
