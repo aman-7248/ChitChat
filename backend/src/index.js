ac4260cdbcd7f08eaa09d5bf7af77b523258dca1
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2025-07-25 19:42:55 +0530

    resolve deployment issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,6 +24,18 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
+
+
+
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes);
+
+
 //3
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
@@ -37,17 +49,6 @@ if(process.env.NODE_ENV==="production"){
 
 }
 
-
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-}));
-
-
-
-
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes);
 
 
 
